077c1a1d4a8f748a82f501770cc48a70db748ec0
Jaewon Hur <hurjaewon@snu.ac.kr>
2023-01-25 10:54:50 +0900

    Fix elf2hex url

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install elf2hex
-git clone git://github.com/sifive/elf2hex.git
+git clone https://github.com/sifive/elf2hex.git
 pushd elf2hex > /dev/null
 autoreconf -i
 ./configure --target=riscv64-unknown-elf
